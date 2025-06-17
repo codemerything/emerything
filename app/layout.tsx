5674af1c466896c1423e88873a2c82f0dfd6c3f8
Emery <mremmanueledem@gmail.com>
2025-06-17 20:25:27 +0100

    change font to geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import { Geist } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Header } from "@/components/header"
 
-const inter = Inter({ subsets: ["latin"] })
 const geist = Geist({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
